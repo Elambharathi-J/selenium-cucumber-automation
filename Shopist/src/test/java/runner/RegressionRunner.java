8562d2a84360a60642ebcf3bcf6fe2207f72ecf0
Elambharathi J <elambharathij@gmail.com>
2026-02-04 12:43:36 +0530

    Adding maven plugins

---
+++ Shopist/src/test/java/runner/RegressionRunner.java
@@ -0,0 +1,18 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/featurefile"},
+        glue = {"stepdefinition"},
+        tags = "@staticDropdown",
+        plugin = {"pretty",
+                "html:target/testoutput/report.html",
+                "json:target/testoutput/report.json",
+                "junit:target/testoutput/report.xml"}
+)
+public class RegressionRunner {
+}
